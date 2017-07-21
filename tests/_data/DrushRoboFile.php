6581885216f5852cb290796de8504b77079173e2
Andor Dávid <andor.david@cheppers.com>
2017-07-22 01:31:38 +0200

    Update dependencies

--- tests/_data/DrushRoboFile.php
+++ tests/_data/DrushRoboFile.php
@@ -1,5 +1,7 @@
 <?php
 
+use Robo\Contract\TaskInterface;
+
 // @codingStandardsIgnoreStart
 class DrushRoboFile extends \Robo\Tasks
 {
@@ -7,12 +9,27 @@ class DrushRoboFile extends \Robo\Tasks
 
     use \Cheppers\Robo\Drush\DrushTaskLoader;
 
-    public function version(string $format = null)
+    public function version(string $format = null): TaskInterface
     {
         return $this
             ->taskDrush('')
             ->setOutput($this->output())
             ->setCmdOption('version', true)
             ->setCmdOption('format', $format);
+    }
+
+    public function coreExecute(
+        array $cmdArguments,
+        array $options = [
+            'process-timeout' => 0
+        ]
+    ): TaskInterface {
+        settype($options['process-timeout'], 'integer');
+
+        return $this
+            ->taskDrush('core-execute')
+            ->setOutput($this->output())
+            ->setProcessTimeout($options['process-timeout'] ?? null)
+            ->setCmdArguments($cmdArguments);
     }
 }
--- tests/acceptance/Task/DrushTaskCest.php
+++ tests/acceptance/Task/DrushTaskCest.php
@@ -8,7 +8,7 @@ class DrushTaskCest
 {
     protected $drushVersion = '8.1.12';
 
-    public function drushVersion(AcceptanceTester $i)
+    public function drushVersion(AcceptanceTester $i): void
     {
         $id = 'version';
         $i->runRoboTask($id, \DrushRoboFile::class, 'version');
@@ -19,11 +19,21 @@ class DrushTaskCest
         );
     }
 
-    public function drushVersionYaml(AcceptanceTester $i)
+    public function drushVersionYaml(AcceptanceTester $i): void
     {
         $id = 'version yaml';
         $i->runRoboTask($id, \DrushRoboFile::class, 'version', 'yaml');
         $i->assertEquals(0, $i->getRoboTaskExitCode($id));
         $i->assertContains($this->drushVersion, $i->getRoboTaskStdOutput($id));
+    }
+
+    public function drushCoreExecute(AcceptanceTester $i): void
+    {
+        $id = '';
+        $i->wantToTest('Process timeout');
+        $i->runRoboTask($id, \DrushRoboFile::class, 'core:execute', '--process-timeout=1', 'sleep', '3');
+        $i->assertEquals(1, $i->getRoboTaskExitCode($id));
+        $i->assertEquals('', $i->getRoboTaskStdOutput($id));
+        $i->assertContains('exceeded the timeout of 1 seconds', $i->getRoboTaskStdError($id));
     }
 }
